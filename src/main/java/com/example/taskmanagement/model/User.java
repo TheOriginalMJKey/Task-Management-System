21f53433937f4bea65878e363f9d5da7b884ae45
TheOriginalMJKey <141678058+TheOriginalMJKey@users.noreply.github.com>
2024-11-18 12:47:32 +0300

    Update User.java

--- src/main/java/com/example/taskmanagement/model/User.java
+++ src/main/java/com/example/taskmanagement/model/User.java
@@ -15,7 +15,6 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Task> tasks;
 
-    // Getters and Setters
     public Long getId() {
         return id;
     }
